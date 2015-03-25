da6cd987e12e1523a5e62eb5bc8436e03fd4db62
BaiGanio <big_l@abv.bg>
2015-03-25 15:35:58 +0200

    Тука съм вкарал част от "лошите",които ще тествам!! По-късно като сработят ще ги изтрия

---
+++ CanvasGame/mainTestEnemies.js
@@ -0,0 +1,53 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+var keys = new Input();
+attachListeners(keys);
+
+
+var yosemity = new Player(20, 390);
+
+
+function update() {
+    this.tick();
+    this.render(ctx);
+    requestAnimationFrame(update);
+};
+
+function tick() {
+    if(keys.right) {
+        yosemity.movement.right = true;
+    } else {
+        yosemity.movement.right = false;
+    };
+
+    if(keys.left) {
+        yosemity.movement.left = true;
+    }else {
+        yosemity.movement.left = false;
+    };
+
+    if(keys.up) {
+        yosemity.movement.up = true;
+    }else {
+        yosemity.movement.up = false;
+    };
+
+    if(keys.down) {
+        yosemity.movement.down = true;
+    }else {
+        yosemity.movement.down = false;
+    }
+
+    yosemity.update();
+
+};
+
+function render(ctx) {
+    //clear the animation
+    ctx.clearRect(0, 0 , canvas.width, canvas.height);
+    yosemity.render(ctx);
+
+};
+
+update();
